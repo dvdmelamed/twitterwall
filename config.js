63f26c3140d4d4bd1d607c5fc2ecfd9075adc4b7
David Melamed <dmelamed@cloudlock.com>
2016-05-02 06:53:52 +0300

    Fix config

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ var config = {
 
   timings: {
     showNextScheduleEarlyBy: '10m', // show the next schedule 10 minutes after the previous starts
-    defaultNoticeHoldTime: '15s',
-    defaultNoticeInterval: '10s',
+    defaultNoticeHoldTime: '30s',
+    defaultNoticeInterval: '5m',
     initialNoticeDelay: '10s',
     showTweetsEvery: '3s'
   }
